644ceea2472f91e8ca331543d5b761c574d366c1
Hir0Yat0 <xToppyThailandx@gmail.com>
2025-03-19 18:53:32 +0700

    chore: run linter on qrcodes_controller_test.rb

--- test/controllers/qrcodes_controller_test.rb
+++ test/controllers/qrcodes_controller_test.rb
@@ -4,4 +4,22 @@ class QrcodesControllerTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
+
+  test "should not be able to show qr by non-students" do
+    [ :teacherA, :adminA, :one ].each do |user|
+      sign_in(user)
+      get qrcodes_url
+      assert_redirected_to root_path
+      assert_equal "You must be a student to access requested page.", flash[:alert]
+    end
+  end
+
+  test "should not be able to access qr scanner by non-teacher" do
+    [ :studentA, :adminA, :one ].each do |user|
+      sign_in(user)
+      get scan_qr_url
+      assert_redirected_to root_path
+      assert_equal "You must be a teacher to access requested page.", flash[:alert]
+    end
+  end
 end
