97e691b54f214964f30cc2aea06500664e1b4a55
Hir0Yat0 <xToppyThailandx@gmail.com>
2025-03-19 19:13:47 +0700

    test: add basic roles tests to home controller tests

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -4,14 +4,23 @@ class HomeControllerTest < ActionDispatch::IntegrationTest
   test "should get index when logged in" do
     sign_in(:adminA)
     get root_path
-    assert status, 200
+    assert_equal status, 200
     assert_includes @response.body, "Total Students"
   end
 
-  # test "not logged in should get redirected to login" do
-  #   get root_path
-  #   assert_redirected_to new_session_path
-  # end
+  test "not logged in should get redirected to login" do
+    get root_path
+    assert_redirected_to new_session_path
+  end
+
+  test "should get home access by all roles" do
+    [ :studentA, :adminA, :one, :studentA ].each do |user|
+      sign_in(user)
+      get root_path
+      assert_equal 200, status
+      assert_includes @response.body, "<title>Dashboard</title>"
+    end
+  end
 
   # private
 
